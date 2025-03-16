78bdb1bc3a0fdd20b15b6555d688f32ca01d147a
linyuhao8 <linhao8896@gmail.com>
2025-03-16 13:29:54 +0800

    small adjust

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default async function HomePage() {
     <div>
     <Navbar />
     <div className="bg-white text-gray-900 py-12 px-6 sm:px-8">
-    <h1 className="text-4xl font-bold text-center mb-10">文章列表</h1>
+    <h1 className="text-4xl font-bold text-center mb-10">Posts List</h1>
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl mx-auto">
       {articles.map((article) => (
         <div key={article.id} className="bg-white rounded-lg shadow-sm hover:shadow-md transition duration-300 ease-in-out">
